ff4468c476647250b4ed1e2fc04bdebc4a8a6635
Arman <zhakypbekov.arman@gmail.com>
2019-10-26 01:38:18 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user"
+};
+
+  const MESSAGE_ENPOINT = 'https://lumbar-slicer.glitch.me';
 
 function App() {
+  const webcamRef = React.useRef(null);
+ 
+  const capture = React.useCallback(
+    () => {
+      const imageSrc = webcamRef.current.getScreenshot();
+      console.log(imageSrc)
+
+      fetch(MESSAGE_ENPOINT, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        message: imageSrc,
+      }),
+    });
+
+    [webcamRef]
+  );
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Webcam
+        audio={false}
+        height={720}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={1280}
+        videoConstraints={videoConstraints}
+      />
+      <button onClick={capture}>Capture photo</button>
+    </>
   );
 }
 
